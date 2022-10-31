dcceea2456f762d8018355d004da84265f8a6083
isavrei <illia.savrei@dxc.com>
2022-10-31 18:37:46 +0100

    mock added, api changes, favorite added, image component fixes

--- weather-widget/src/app/pages/main-page/main-page.component.ts
+++ weather-widget/src/app/pages/main-page/main-page.component.ts
@@ -1,8 +1,8 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ApiService } from "../../services/api.service";
 import { GeolocationService } from "../../services/geolocation.service";
-import { Observable } from "rxjs";
-import { WeatherResponse } from "../../domain";
+import { Observable, of } from "rxjs";
+import { Clouds, CustomGeolocation, MainWeatherParams, System, Weather, WeatherResponse, Wind } from "../../domain";
 
 @Component({
   selector: 'app-main-page',
@@ -21,14 +21,15 @@ export class MainPageComponent {
 
   constructor(private api: ApiService,
               private geolocationService: GeolocationService) {
-    geolocationService.getLocationOnce().then(geolocation => {
-      const weatherResponse$ = this.api.getWeatherByCoords({
-        lat: geolocation.coords.latitude,
-        lon: geolocation.coords.longitude
-      });
-      this.citiesList.push(weatherResponse$);
-      console.log(this.citiesList)
-    }).catch(console.warn);
+    this.getCitiesListMock();
+    // geolocationService.getLocationOnce().then(geolocation => {
+    //   const weatherResponse$ = this.api.getWeatherByCoords({
+    //     lat: geolocation.coords.latitude,
+    //     lon: geolocation.coords.longitude
+    //   });
+    //   this.citiesList.push(weatherResponse$);
+    //   console.log(this.citiesList)
+    // }).catch(console.warn);
   }
 
   onAddressChange(address: any) {
@@ -40,8 +41,43 @@ export class MainPageComponent {
         })
       );
       if (prevLength < this.citiesList.length) {
-        this.el ? this.el.nativeElement.value = '' : undefined;
+        this.el ? this.el.nativeElement.value = '' : this.el;
       }
     }
+  }
+
+  getCitiesListMock() {
+    let mock = new WeatherResponse({
+      weather: new Array<Weather>(new Weather({
+        id: 1,
+        main: 'main',
+        description: 'description',
+        icon: 'assets/loading.gif'
+      })),
+      coord: new CustomGeolocation({
+        lat: 15,
+        lon: 45,
+      }),
+      base: 'base',
+      main: new MainWeatherParams({
+        temp: 15,
+        feels_like: 17,
+        temp_max: 20,
+        temp_min: 12,
+        pressure: 1000
+      }),
+      visibility: 10000,
+      wind: new Wind(),
+      clouds: new Clouds(),
+      dt: 10,
+      sys: new System(),
+      timezone: 3600,
+      name: 'cityName',
+      cod: '200'
+    });
+    for (let i = 0; i < 5; i++) {
+      this.citiesList.push(of({ ...mock, name: 'cityName' + i }));
+    }
+    return this.citiesList
   }
 }
